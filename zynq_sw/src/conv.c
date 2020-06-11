c67a8007738de4980f8ad4e16ec170eb6a8634f3
tb_designs <ryanblais@uvic.ca>
2020-06-10 21:10:39 -0700

    finished conv2d function

--- zynq_sw/src/conv.c
+++ zynq_sw/src/conv.c
@@ -31,9 +31,11 @@ SOFTWARE.
 #define pixel char //8-bit images, change if 14-bit
 
 /*Image Dimensions*/
-#define M 120
-#define N 160
+#define r_dim 120
+#define c_dim 160
 
+/*Kernel Dimensions*/
+#define k_dim 3 //Unless otherwise specified, kernel is 3x3
 
 /*******************
  *    FUNCTIONS    *
@@ -42,28 +44,60 @@ SOFTWARE.
 /*******************************************************
  *    Convolve 2D
  *    Inputs:  A - 120x160 Image Matrix
+ *             C - 120x160 Result Matrix
  *             K - 3x3 Convolution Kernel
  * 
- *    Outputs: c - Pointer to resulting C matrix
+ *    Outputs: C - Modified Result Matrix
 *******************************************************/
 
-pixel* conv2d(pixel A, pixel K) {
-  static pixel C[M][N];
+pixel conv2d(pixel A, pixel C, pixel K) {
+    short rows = r_dim;
+    short cols = c_dim;
+    short a, b, i, j, k, sum;
 
+    short max = 255; //Maximum pixel value
 
-
+    /*Iterate through image*/
+    for (i=0 ; 1< rows ; i++) {
+        for (j=0 ; j < cols ; j++){
+            sum = 0;
+            /* Iterate through kernel */
+            for (a=-1; a < 2; a++) {
+                /*Check Row Indecies for out of bounds*/
+                if (i+a < 0 || i+a > r_dim - 1) {
+                    continue;
+                } else {
+                    for (b=-1; b < 2; b++) {
+                        /*Check Column Indecies for out of bounds*/
+                        if ( j+b < 0 || j+b > c_dim -1) {
+                            continue;
+                        } else {
+                            /*Add to the sum*/
+                            sum = sum + A[i+a][j+b]*K[a+1][b+1];
+                        }
+                    }
+                }
+            }
+            /*Minor Thresholding*/
+            if (sum < 0) sum = 0;
+            if (sum > max) sum = max;
+            C[i][j] = sum;
+        } 
+    }
+    return C;
 };
 
 
 /*******************************************************
  *    Seperable Convolve 2D
  *    Inputs:  A - 120x160 Image Matrix
+ *             C - 120x160 Result Matrix
  *             K - 3x3 Seperable Convolution Kernel
  * 
- *    Outputs: c - Pointer to resulting C matrix
+ *    Outputs: C - Modified Result Matrix
 *******************************************************/
 
-pixel* sep_conv2d(pixel A, pixel K) {
+pixel* sep_conv2d(pixel A[M][N], pixel K[k_dim][k_dim]) {
 
 };
 
