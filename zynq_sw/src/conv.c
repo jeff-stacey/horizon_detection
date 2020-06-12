c2875bbeb447a810b066c904e2547d1bea5bcfae
tb_designs <ryanblais@uvic.ca>
2020-06-12 14:46:59 -0700

    Added hypot, theta, and edge2bin functions

--- zynq_sw/src/conv.c
+++ zynq_sw/src/conv.c
@@ -24,13 +24,12 @@ SOFTWARE.
 #include <math.h>
 
 /*TODO: HELPER FUNCTIONS FOR EDGE DETECTION
-    -2d convolve DONE (may have scaling issues in output around edges)
-    -seperable 2d convolve
-    -1d convolve
-    - read image?
+    - DONE 2d convolve (may have scaling issues in output around edges)
+    - 1d convolve with image
+    - read image into array?
     - grey edge map to binary edge map conversion
-    - element-wise array "hypotenuse" operation
-    - element-wise array "theta" operation
+    - DONE element-wise array "hypotenuse" operation
+    - DONE element-wise array "theta" operation
     - Non-Max suppression (rquires edge map and theta matrix)
     - Double Thresholding
     - Edge Tracking by Hysterysis
@@ -46,16 +45,20 @@ SOFTWARE.
 /*Image Dimensions*/
 #define r_dim 120
 #define c_dim 160
+#define num_pix r_dim*c_dim
 
 /*Kernel Dimensions*/
 #define k_dim 3 //Unless otherwise specified, kernel is 3x3
+
+/*Testing Trigger*/
+#define TEST 1
 
 /*******************
  *    FUNCTIONS    *
 ********************/
 
 /*******************************************************
- *    Convolve 2D
+ *    2D Convolution
  *    Inputs:  A - 120x160 Image Matrix
  *             C - 120x160 Result Matrix
  *             K - 3x3 Convolution Kernel
@@ -98,14 +101,14 @@ void conv2d(pixel A[r_dim][c_dim], pixel C[r_dim][c_dim], pixel K[k_dim][k_dim])
 };
 
 /********************************************************************
- *    Seperable Convolve 2D
+ *    1D Convolution
  *    Inputs:  A - 120x160 Image Matrix
  *             C - 120x160 Result Matrix
  *             K_c - 3x1 Seperated Section of Kernel (column vector)
  *             K_r - 1x3 Seperated Section of Kernel (row vector)
  * 
 **********************************************************************/
-void sep_conv2d(pixel A[r_dim][c_dim], pixel K_r[k_dim], pixel K_c[k_dim], pixel C[r_dim][c_dim]) {
+void conv1d(pixel A[r_dim][c_dim], pixel K_r[k_dim], pixel K_c[k_dim], pixel C[r_dim][c_dim]) {
     //TODO
 };
 
@@ -113,18 +116,18 @@ void sep_conv2d(pixel A[r_dim][c_dim], pixel K_r[k_dim], pixel K_c[k_dim], pixel
  *    Hyptotenuse Calculation (element-wise)
  *    Inputs:  X - First array operand
  *             Y - Second array operand
- *             C - Float-type Result Matrix
+ *             C - Result Matrix
  *             r - # of rows in matricies
  *             c - # of columns in matricies
  * 
 **********************************************************************/
-void img_hypot (pixel X[], pixel Y[], pixel C[], short r, short c) {
+void img_hypot (pixel X[r_dim][c_dim], pixel Y[r_dim][c_dim], pixel C[r_dim][c_dim]) {
 
     short i, j;
     double result;
 
-    for (i=0; i < r-1 ; i++) {
-        for (j=0; j < c-1 ; j++) {
+    for (i=0; i < r_dim-1 ; i++) {
+        for (j=0; j < c_dim-1 ; j++) {
             result =  hypot((double)X[i][j], (double)Y[i][j]);
             C[i][j] = (pixel)result;
         };
@@ -135,27 +138,93 @@ void img_hypot (pixel X[], pixel Y[], pixel C[], short r, short c) {
  *    Angle Calculation (element-wise)
  *    Inputs:  X - First array operand
  *             Y - Second array operand
- *             C - Float-type Result Matrix
+ *             C - Result Matrix
  *             r - # of rows in matricies
  *             c - # of columns in matricies
  * 
 **********************************************************************/
-void img_theta (pixel X[], pixel Y[], pixel C[], short r, short c) {
+void img_theta (pixel X[r_dim][c_dim], pixel Y[r_dim][c_dim], pixel C[r_dim][c_dim]) {
 
     short i, j;
     double result;
 
-    for (i=0; i < r-1 ; i++) {
-        for (j=0; j < c-1 ; j++) {
+    for (i=0; i < r_dim-1 ; i++) {
+        for (j=0; j < c_dim-1 ; j++) {
             result =  atan((double)Y[i][j]/(double)X[i][j]);
             C[i][j] = (pixel)result;
         };
     };
 };
 
+/********************************************************************
+ *    Edge Map to Binary Edge Map Conversion (element-wise)
+ *    Inputs:  E   - Edge map image
+ *             B   - Result Matrix
+ *             r   - # of rows in matricies
+ *             c   - # of columns in matricies
+ *             u_t - Upper threshold value 
+ * 
+**********************************************************************/
+
+void edge2bin(pixel E[r_dim][c_dim], pixel B[r_dim][c_dim], short u_t) {
+
+    short i, j;
+
+    for (i=0; i < r_dim-1 ; i++) {
+        for (j=0; j < c_dim-1 ; j++) {
+            if (E[i][j] < u_t) {
+                B[i][j] = 0;
+            } else {
+                B[i][j] = 1;
+            }
+        };
+    };
+};
+
+/********************************************************************
+ *    1D Raw Image Data to 2D Image Array
+ *    Inputs: A - 1D C integer array of Raw Image Data 
+ *            w - Width of the output image ( in pixels)
+ *            h - Height of the output image ( in pixels)
+ * 
+ *    Outputs: C - 2D C array of Image data (char)
+ * 
+**********************************************************************/
+pixel raw2arr(int A[num_pix], short w, short h) {
+
+    pixel C[w][h];
+    short i, r_idx, c_idx;
+
+    r_idx = 0;
+    for(i = 0; i < num_pix-1; i++){
+        /*Check for end or row*/
+        c_idx = i%w;
+        if (c_idx == 0) {
+            /*Go to next row*/
+            r_idx++;
+        }
+        C[r_idx][c_idx] = (pixel)A[i];
+    }
+
+    return C;
+};
+
 int main() {
 
-    print("Hello world\n\r");
+    if (TEST) {
+        print("Helper Function Testing Start\n\r");
+
+        /*Create/Import test image as C array*/
+        print("Importing Test Image\n\r");
+
+        /*Perform Helper Operations*/
+        print("Applying functions to image\n\r");
+
+        /*Save results into files for comparison*/
+        print("Saving results\n\r");
+
+
+    }
 
     return 0;
 }
