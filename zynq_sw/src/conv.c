ecf334c8192381a4c04649ce4f436985dfdf3ee4
tb_designs <ryanblais@uvic.ca>
2020-06-12 12:50:01 -0700

    added img_hypot and img_theta functions added comment list of helper functions to implement minor typo fixes to edge_detection.py

--- zynq_sw/src/conv.c
+++ zynq_sw/src/conv.c
@@ -23,6 +23,19 @@ SOFTWARE.
 #include <stdio.h>
 #include <math.h>
 
+/*TODO: HELPER FUNCTIONS FOR EDGE DETECTION
+    -2d convolve DONE (may have scaling issues in output around edges)
+    -seperable 2d convolve
+    -1d convolve
+    - read image?
+    - grey edge map to binary edge map conversion
+    - element-wise array "hypotenuse" operation
+    - element-wise array "theta" operation
+    - Non-Max suppression (rquires edge map and theta matrix)
+    - Double Thresholding
+    - Edge Tracking by Hysterysis
+*/
+
 /*******************
  *    DEFINES      *
 ********************/
@@ -46,11 +59,9 @@ SOFTWARE.
  *    Inputs:  A - 120x160 Image Matrix
  *             C - 120x160 Result Matrix
  *             K - 3x3 Convolution Kernel
- * 
- *    Outputs: C - Modified Result Matrix
+ *
 *******************************************************/
-
-pixel conv2d(pixel A, pixel C, pixel K) {
+void conv2d(pixel A[r_dim][c_dim], pixel C[r_dim][c_dim], pixel K[k_dim][k_dim]) {
     short rows = r_dim;
     short cols = c_dim;
     short a, b, i, j, k, sum;
@@ -78,30 +89,69 @@ pixel conv2d(pixel A, pixel C, pixel K) {
                     }
                 }
             }
-            /*Minor Thresholding*/
+            /*Sum Thresholding*/
             if (sum < 0) sum = 0;
             if (sum > max) sum = max;
             C[i][j] = sum;
         } 
     }
-    return C;
 };
 
-
-/*******************************************************
+/********************************************************************
  *    Seperable Convolve 2D
  *    Inputs:  A - 120x160 Image Matrix
  *             C - 120x160 Result Matrix
- *             K - 3x3 Seperable Convolution Kernel
+ *             K_c - 3x1 Seperated Section of Kernel (column vector)
+ *             K_r - 1x3 Seperated Section of Kernel (row vector)
  * 
- *    Outputs: C - Modified Result Matrix
-*******************************************************/
-
-pixel* sep_conv2d(pixel A[M][N], pixel K[k_dim][k_dim]) {
-
+**********************************************************************/
+void sep_conv2d(pixel A[r_dim][c_dim], pixel K_r[k_dim], pixel K_c[k_dim], pixel C[r_dim][c_dim]) {
+    //TODO
 };
 
+/********************************************************************
+ *    Hyptotenuse Calculation (element-wise)
+ *    Inputs:  X - First array operand
+ *             Y - Second array operand
+ *             C - Float-type Result Matrix
+ *             r - # of rows in matricies
+ *             c - # of columns in matricies
+ * 
+**********************************************************************/
+void img_hypot (pixel X[], pixel Y[], pixel C[], short r, short c) {
 
+    short i, j;
+    double result;
+
+    for (i=0; i < r-1 ; i++) {
+        for (j=0; j < c-1 ; j++) {
+            result =  hypot((double)X[i][j], (double)Y[i][j]);
+            C[i][j] = (pixel)result;
+        };
+    };
+};
+
+/********************************************************************
+ *    Angle Calculation (element-wise)
+ *    Inputs:  X - First array operand
+ *             Y - Second array operand
+ *             C - Float-type Result Matrix
+ *             r - # of rows in matricies
+ *             c - # of columns in matricies
+ * 
+**********************************************************************/
+void img_theta (pixel X[], pixel Y[], pixel C[], short r, short c) {
+
+    short i, j;
+    double result;
+
+    for (i=0; i < r-1 ; i++) {
+        for (j=0; j < c-1 ; j++) {
+            result =  atan((double)Y[i][j]/(double)X[i][j]);
+            C[i][j] = (pixel)result;
+        };
+    };
+};
 
 int main() {
 
