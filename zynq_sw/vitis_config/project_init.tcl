1f051e840ec4d3f356ffc971ddf947025a4027a3
Jeff Stacey <jtst@uvic.ca>
2020-06-21 21:42:26 -0700

    More tweaks to the vitis config script

--- zynq_sw/vitis_config/project_init.tcl
+++ zynq_sw/vitis_config/project_init.tcl
@@ -3,29 +3,37 @@
 set app_name "horizon_detection"
 
 # Create the workspace
-if {$tcl_platform(platform) == "unix"} {
-    puts "Creating workspace directory (unix)"
-    exec mkdir -p workspace
+if [file exist workspace] {
+    puts "Workspace already exists, so I'll leave it alone and just re-import the source files."
+    puts "If you just checked out a new branch, it's a good idea to regenerate the workspace."
+    puts "To do that, delete the workspace directory and run this script again."
+    
+    setws workspace
+    cd workspace
 } else {
-    puts "Creating workspace directory"
-    exec if not exist \"workspace\" mkdir \"workspace\"
-}
-setws workspace
-cd workspace
+    puts "workspace not found - creating it"
+    if {$tcl_platform(platform) == "unix"} {
+        puts "Creating workspace directory (unix)"
+        exec mkdir -p workspace
+    } else {
+        puts "Creating workspace directory"
+        exec if not exist \"workspace\" mkdir \"workspace\"
+    }
 
-# Set up the platform
-puts "Creating the platform"
-platform create -name "zcu104" -hw ../vitis_config/zcu104.xsa -os standalone -proc psu_cortexr5_0
+    setws workspace
+    cd workspace
 
-# Create our app for the Cortex R5
-puts "Creating R5 app"
-if [ catch { app create -name $app_name -platform zcu104 -proc psu_cortexr5_0 -os standalone -template {Empty Application} } ] {
-    puts "R5 App already exists. Skipping app creation. If you really need to regenerate the app, delete the workspace directory and run this again."
-} else {
-}
+    # Set up the platform
+    puts "Creating the platform"
+    platform create -name "zcu104" -hw ../vitis_config/zcu104.xsa -os standalone -proc psu_cortexr5_0
 
-puts "Adding linker flag for math library"
-if [ catch { app config -name $app_name -add libraries {m} } ] {
+    # Create our app for the Cortex R5
+    puts "Creating R5 app"
+    app create -name $app_name -platform zcu104 -proc psu_cortexr5_0 -os standalone -template {Empty Application}
+
+    puts "Adding linker flag for math library"
+    if [ catch { app config -name $app_name -add libraries {m} } ] {
+    }
 }
 
 # Import source files
